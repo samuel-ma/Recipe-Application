26144d3051e38ea7ca81fdd913b4f9f1c6c7b75c
Samuel Majok <82509653+SamiiE99@users.noreply.github.com>
2022-07-17 20:30:07 +0200

    another commit

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -2,12 +2,19 @@
 
 import styled from "styled-components";
 import {useState} from "react";
-import {FaSearch} from "react-icons/fa";
+import {BiSearch} from "react-icons/bi";
 
 function Search() {
   return (
     <FormStyle>
+      <div className="search-icon">
+        <BiSearch className="search" />
+      </div>
+
+      <div className="input-div">
         <input type="text" />
+      </div>
+      
     </FormStyle>
   )
 }
@@ -16,12 +23,53 @@ const FormStyle = styled.div`
   display:flex;
   align-items: center;
   justify-content: center;
+  border-radius: 5px;
 
   input{
+    height: 100%;
     outline: none;
-    width: 40%;
-    height: 2rem;
+    border: none;
+    width: 300px;
+    background: transparent;
   }
+
+  .search{
+    color: white;
+  }
+
+  .search-icon{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: linear-gradient(35deg, blue, lightblue);
+    border-radius: 30px;
+    padding-right: 20px;
+    padding-left: 20px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    margin-right: -20px;
+    z-index: 1;
+  }
+
+  .search-icon:hover{
+    cursor: pointer;
+    background-image: linear-gradient(to right, #f27121, #e94057);
+  }
+
+  .input-div{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    outline: none;
+    border-radius: 5px;
+    height: 34px;
+    border-width: 0.5px;
+    border-color: blue;
+    border-style: dashed;
+    border-radius: 20px;
+    padding-left: 25px;
+  }
+    
 
 `;
 
